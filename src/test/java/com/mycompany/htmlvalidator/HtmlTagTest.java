1440c39ed9269879f48d9953be95ec923a5c569e
Carl McGraw <cjmcgraw@uw.edu>
2014-05-29 11:50:55 -0700

    Add intial test stubs to project for HtmlTag and HtmlValidator

---
+++ src/test/java/com/mycompany/htmlvalidator/HtmlTagTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.htmlvalidator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HtmlTagTest {
+    
+    @Test
+    public void testGetElement() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    @Test
+    public void testIsOpenTag() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    @Test
+    public void testIsSelfClosing() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    @Test
+    public void testEqualsHtmlTag() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    @Test
+    public void testMatches() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+}
---
+++ src/test/java/com/mycompany/htmlvalidator/HtmlValidatorTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.htmlvalidator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HtmlValidatorTest {
+    
+    @Test
+    public void testAddTag() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    @Test
+    public void testRemoveAll() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+    @Test
+    public void testValidate() {
+        fail("Not yet implemented"); // TODO
+    }
+    
+}
