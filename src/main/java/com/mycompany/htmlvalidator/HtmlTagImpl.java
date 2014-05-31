3363443ef4148347c5c5f5c3a46e2f7052388b0a
Carl McGraw <cjmcgraw@uw.edu>
2014-05-30 21:08:43 -0700

    Implement basic functionality for HtmlTagImpl to pass HtmlTagTest

--- src/main/java/com/mycompany/htmlvalidator/HtmlTagImpl.java
+++ src/main/java/com/mycompany/htmlvalidator/HtmlTagImpl.java
@@ -1,34 +1,60 @@
 package com.mycompany.htmlvalidator;
 
+import java.util.Set;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.regex.Matcher.*;
+
 import com.mycompany.htmlvalidator.interfaces.HtmlTag;
 
 public class HtmlTagImpl implements HtmlTag{
+    public final static Set<String> selfClosingTags = new HashSet<String>(
+            Arrays.asList("area","base", "br", "col", "command", "hr", "img",
+                          "input", "keygen","link", "meta", "param", "source", 
+                          "track", "wbr")
+            );
+    private String element;
+    private boolean isClosing;
+    private boolean selfClosing;
+    
+    public HtmlTagImpl(String tagData){
+        // TODO set up constructor
+    }
 
     @Override
     public String getElement() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.element;
     }
 
     @Override
     public boolean isOpenTag() {
-        // TODO Auto-generated method stub
-        return false;
+        return !this.isClosing;
     }
 
     @Override
     public boolean isSelfClosing() {
-        return false;
+        return this.selfClosing;
     }
 
     @Override
     public boolean equals(HtmlTag other) {
-        return false;
+        return this.elementsMatch(other) && this.tagsMatch(other);
     }
 
     @Override
     public boolean matches(HtmlTag other) {
         return false;
+    }
+    
+    private boolean elementsMatch(HtmlTag other) {
+        return this.element == other.getElement();
+    }
+    
+    private boolean tagsMatch(HtmlTag other) {
+        if (this.isSelfClosing()) {
+            return this.isSelfClosing() == other.isSelfClosing();
+        }
+        return this.isOpenTag() && !other.isOpenTag();
     }
     
 }
