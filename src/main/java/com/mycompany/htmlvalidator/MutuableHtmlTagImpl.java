51c9a15212eaa441d17953c673febde05d924838
Carl McGraw <cjmcgraw@uw.edu>
2014-06-01 18:56:07 -0700

    Updated HtmltagImpl to be MutuableHtmlTagImpl to reflect the Mutuability of the tags at the point of creation. Further implemented the MutuableHtmlTag interface. MutuableHtmlTag still holds a super interface to HtmlTag.

--- src/main/java/com/mycompany/htmlvalidator/MutuableHtmlTagImpl.java
+++ src/main/java/com/mycompany/htmlvalidator/MutuableHtmlTagImpl.java
@@ -5,23 +5,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import com.mycompany.htmlvalidator.interfaces.HtmlTag;
+import com.mycompany.htmlvalidator.interfaces.MutuableHtmlTag;
 
-public class HtmlTagImpl implements HtmlTag{
+public class MutuableHtmlTagImpl implements MutuableHtmlTag{
     public final static Set<String> selfClosingTags = new HashSet<String>(
             Arrays.asList("area","base", "br", "col", "command", "hr", "img",
                           "input", "keygen","link", "meta", "param", "source", 
                           "track", "wbr")
             );
-    private String element;
-    private boolean isClosing;
     private boolean selfClosing;
+    private boolean isClosing;
+    private String element;
     
-    public HtmlTagImpl(String element, boolean isClosing){
-        this.element = element;
-        this.selfClosing = selfClosingTags.contains(element);
-        this.isClosing = selfClosing || isClosing;
-    }
-
     @Override
     public String getElement() {
         return this.element;
@@ -45,6 +40,20 @@ public class HtmlTagImpl implements HtmlTag{
     @Override
     public boolean matches(HtmlTag other) {
         return this.elementsEqual(other) && this.oppositeTag(other);
+    }
+    
+    public void setIsOpenTag(boolean openTag) {
+        this.setState(this.element, !openTag);
+    }
+    
+    public void setElement(String element) {
+        this.setState(element, this.isClosing);
+    }
+    
+    private void setState(String element, boolean isClosing) {
+        this.element = element;
+        this.selfClosing = selfClosingTags.contains(element);
+        this.isClosing = this.selfClosing || isClosing;
     }
     
     private boolean elementsEqual(HtmlTag other) {
