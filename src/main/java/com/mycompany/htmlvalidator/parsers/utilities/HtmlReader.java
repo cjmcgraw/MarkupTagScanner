51a30aac81b1633efe12af07c59fbd807534430c
Carl McGraw <cjmcgraw@uw.edu>
2014-06-06 06:01:22 -0700

    Provided HtmlReader interface that defines the HtmlBufferedReader that will be implemented at a later time. This has been defined now to provide the HtmlbufferedReaderMock objects functionality.

---
+++ src/main/java/com/mycompany/htmlvalidator/parsers/utilities/HtmlReader.java
@@ -0,0 +1,8 @@
+package com.mycompany.htmlvalidator.parsers.utilities;
+
+import java.io.Closeable;
+import java.util.Iterator;
+
+public interface HtmlReader extends Iterator<String>, Closeable{
+    
+}
