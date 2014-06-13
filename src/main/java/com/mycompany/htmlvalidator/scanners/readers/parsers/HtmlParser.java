666435a91ad699ccba423a0626ba591dc5473ce3
Carl McGraw <cjmcgraw@uw.edu>
2014-06-13 11:14:29 -0700

    Updated HtmlParser into abstract class so that it may implement static methods useful for parsers to know.

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlParser.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlParser.java
@@ -1,11 +1,29 @@
 package com.mycompany.htmlvalidator.scanners.readers.parsers;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.mycompany.htmlvalidator.scanners.readers.utilities.PushbackAndPositionReader;
 
-public interface HtmlParser<T> {
+public abstract class HtmlParser {
+    public static final char OPEN_TAG_ENCLOSURE = '<';
+    public static final char CLOSE_TAG_ENCLOSURE = '>';
+    private static final Set<Character> TAG_ENCLOSURES = 
+            new HashSet<Character>(Arrays.asList(OPEN_TAG_ENCLOSURE, CLOSE_TAG_ENCLOSURE));
     
-    public T parse(PushbackAndPositionReader input) throws IOException;
+    public static boolean isOpenTagEnclosure(char c) {
+        return c == OPEN_TAG_ENCLOSURE;
+    }
     
+    public static boolean isCloseTagEnclosure(char c) {
+        return c == CLOSE_TAG_ENCLOSURE;
+    }
+    
+    public static boolean isTagEnclosure(char c) {
+        return TAG_ENCLOSURES.contains(c);
+    }
+    
+    public abstract HtmlData parse(PushbackAndPositionReader input, MutableHtmlData result) throws IOException;
 }
