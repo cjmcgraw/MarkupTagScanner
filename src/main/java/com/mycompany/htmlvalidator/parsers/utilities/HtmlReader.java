1382b53798ffc1d426a7eab2110aefadf652cfdb
Carl McGraw <cjmcgraw@uw.edu>
2014-06-18 02:28:04 -0700

    Removed unnecessary clutter classes.

--- src/main/java/com/mycompany/htmlvalidator/parsers/utilities/HtmlReader.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.htmlvalidator.parsers.utilities;
-
-import java.io.Closeable;
-import java.util.Iterator;
-
-public interface HtmlReader extends Iterator<String>, Closeable{
-    
-    public int getCurrentLineNumber();
-}
