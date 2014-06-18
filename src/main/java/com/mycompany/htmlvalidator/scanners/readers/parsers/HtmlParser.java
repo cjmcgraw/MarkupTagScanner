4d95f758c5de5f2f418f4f63aa50e34ab3eaeffe
Carl McGraw <cjmcgraw@uw.edu>
2014-06-18 02:02:31 -0700

    Updated HtmlParser by removing redundant interface and utilizing protected methods to ensure that any users using the abstract class as an interface will not be able to access encapsulated functionality.

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlParser.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlParser.java
@@ -1,10 +1,90 @@
 package com.mycompany.htmlvalidator.scanners.readers.parsers;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+import com.mycompany.htmlvalidator.scanners.readers.parsers.errors.EndOfInputParsingException;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.errors.UnclosedTagParsingException;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.errors.UnexpectedCloseTagParsingException;
 import com.mycompany.htmlvalidator.scanners.readers.utilities.PushbackAndPositionReader;
 
-public interface HtmlParser {
+public abstract class HtmlParser{
+    public static final char OPEN_TAG_ENCLOSURE = '<';
+    public static final char CLOSE_TAG_ENCLOSURE = '>';
+    public static final char QUOTE_TAG_ENCLOSURE = '"';
+    public static final char CLOSING_TAG = '/';
+    private static final Set<Character> TAG_ENCLOSURES = 
+            new HashSet<Character>(Arrays.asList(OPEN_TAG_ENCLOSURE, CLOSE_TAG_ENCLOSURE));
     
-    public HtmlData parse(PushbackAndPositionReader input, MutableHtmlData result) throws IOException;
+    public static boolean isOpenTagEnclosure(char c) {
+        return c == OPEN_TAG_ENCLOSURE;
+    }
+    
+    public static boolean isCloseTagEnclosure(char c) {
+        return c == CLOSE_TAG_ENCLOSURE;
+    }
+    
+    public static boolean isClosingTag(char c) {
+        return c == CLOSING_TAG;
+    }
+    
+    public static boolean isQuoteTagEnclosure(char c) {
+        return c == QUOTE_TAG_ENCLOSURE;
+    }
+    
+    public static boolean isTagEnclosure(char c) {
+        return TAG_ENCLOSURES.contains(c);
+    }
+    
+    protected PushbackAndPositionReader input;
+    protected MutableHtmlData result;
+    
+    protected char readNext() throws IOException {
+        int value = this.input.read();
+        
+        if (this.isEndOfInput(value))
+            throw new EndOfInputParsingException(this.input.getPosition(), 
+                                                 this.result);
+        return (char) value;
+    }
+    
+    public abstract HtmlData parse(PushbackAndPositionReader input, MutableHtmlData result) throws IOException;
+    
+    protected void unread(char c) throws IOException {
+        this.input.unread(c);
+    }
+    
+    protected boolean isEndOfInput(int value) {
+        return value == -1;
+    }
+    
+    protected boolean validateChar(char c) throws IOException {
+        if(isCloseTagEnclosure(c))
+            this.closeTagRead(c);
+        else if (isOpenTagEnclosure(c))
+            this.openTagRead(c);
+        
+        return true;
+    }
+    
+    protected void closeTagRead(char c) throws IOException {
+        this.unread(c);
+        throw new UnexpectedCloseTagParsingException(this.input.getPosition(), this.result);
+    }
+    
+    protected void openTagRead(char c) throws IOException {
+        this.unread(c);
+        throw new UnclosedTagParsingException(this.input.getPosition(), this.result);
+    }
+    
+    protected void setState(PushbackAndPositionReader input, MutableHtmlData result) {
+        this.input = input;
+        this.result = result;
+    }
+    
+    protected void clearState() {
+        this.setState(null, null);
+    }
 }
