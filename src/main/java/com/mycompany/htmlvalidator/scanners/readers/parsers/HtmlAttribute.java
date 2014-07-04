3f2e54b828f29a42b23b9b8925c96a36e03a13bd
Carl McGraw <cjmcgraw@uw.edu>
2014-07-04 04:57:31 -0700

    Update: Updated HtmlAttribute to test self closing tags and provide proper functionality.

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlAttribute.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlAttribute.java
@@ -8,18 +8,18 @@ public class HtmlAttribute {
     
     public static final String CLOSING_TAG = "" + HtmlParser.CLOSING_TAG;
     public static final String DEFAULT_CLOSING_NAME = "self-closing";
-    public static final String DEFAULT_CLOSING_VALUE = "";
+    public static final String DEFAULT_EMPTY_VALUES = "";
     
     private String attributeName;
     private String attributeValue;
     
     public HtmlAttribute() {
-        this.attributeName = "";
-        this.attributeValue = "";
+        this.attributeName = DEFAULT_EMPTY_VALUES;
+        this.attributeValue = DEFAULT_EMPTY_VALUES;
     }
     
     public HtmlAttribute(String name) {
-        this(name, "");
+        this(name, DEFAULT_EMPTY_VALUES);
     }
     
     public HtmlAttribute(String name, String value) {
@@ -32,9 +32,10 @@ public class HtmlAttribute {
     }
     
     public void setName(String name) {
-        if (name.equals(CLOSING_TAG))
+        if (name.equals(CLOSING_TAG)) {
             this.attributeName = DEFAULT_CLOSING_NAME;
-        else
+            this.setValue(DEFAULT_EMPTY_VALUES);
+        }else
             this.attributeName = name;
     }
     
@@ -44,15 +45,30 @@ public class HtmlAttribute {
     
     public void setValue(String value) {
         if (this.attributeName.equals(DEFAULT_CLOSING_NAME))
-            this.attributeValue = DEFAULT_CLOSING_VALUE;
+            this.attributeValue = DEFAULT_EMPTY_VALUES;
         else
             this.attributeValue = value.trim();
     }
     
     public boolean isFlag() {
-        return this.attributeName != null && this.attributeValue == null;
+        return this.hasName() && !this.hasValue();
     }
     
+    public boolean isClosingFlag() {
+        return this.isFlag() && this.nameIs(DEFAULT_CLOSING_NAME);
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                 + ((attributeName == null) ? 0 : attributeName.hashCode());
+        result = prime * result
+                 + ((attributeValue == null) ? 0 : attributeValue.hashCode());
+        return result;
+    }
+
     public boolean equals(Object other) {
         if (other instanceof HtmlAttribute)
             return equals( (HtmlAttribute) other);
@@ -61,16 +77,30 @@ public class HtmlAttribute {
     }
     
     public boolean equals(HtmlAttribute other) {
-        boolean result = (this.attributeName.equals(other.attributeName) && 
-                          this.attributeValue.equals(other.attributeValue));
+        boolean result = (this.nameIs(other.attributeName) &&
+                          this.valueIs(other.attributeValue));
         return result;
     }
     
     public String toString() {
-        return this.attributeName + ((this.hasAttributeValue()) ? attributeSplitter + this.attributeValue : "");
+        String name = (this.isClosingFlag()) ? CLOSING_TAG : this.attributeName;
+        String value = (this.hasValue()) ? attributeSplitter + this.attributeValue : "";
+        return name + value;
     }
     
-    private boolean hasAttributeValue() {
-        return this.attributeValue != "";
+    private boolean hasValue() {
+        return !this.valueIs(DEFAULT_EMPTY_VALUES);
+    }
+    
+    private boolean hasName() {
+        return !this.nameIs(DEFAULT_EMPTY_VALUES);
+    }
+    
+    private boolean nameIs(String s) {
+        return this.attributeName.equals(s);
+    }
+    
+    private boolean valueIs(String s) {
+        return this.attributeValue.equals(s);
     }
 }
---
+++ src/test/java/com/mycompany/htmlvalidator/scanners/readers/parsers/HtmlAttributeTest.java
@@ -0,0 +1,327 @@
+package com.mycompany.htmlvalidator.scanners.readers.parsers;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class HtmlAttributeTest {
+    public static final String DEFAULT_NAME = "some name";
+    public static final String DEFAULT_VALUE = "some value";
+    public static final String EMPTY_VALUE = HtmlAttribute.DEFAULT_EMPTY_VALUES;
+    public static final String CLOSING_NAME = HtmlAttribute.CLOSING_TAG;
+    
+    private HtmlAttribute attribute;
+    
+    @Before
+    public void initializeState() {
+        this.setState(DEFAULT_NAME);
+        this.attribute.setValue(DEFAULT_VALUE);
+    }
+    
+    @Test
+    public void testNameConstructor_ReturnsCorrectName() {
+        // Arrange
+        String expData = "constructed name";
+        String data;
+        
+        this.setState(expData);
+        
+        // Apply
+        data = this.attribute.getName();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testNameConstructor_ReturnsEmptyValue() {
+        // Arrange
+        String expData = "";
+        String data;
+        
+        this.setState("constructed name");
+        
+        // Apply
+        data = this.attribute.getValue();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testSetName_ReturnsCorrectName() {
+        // Arrange
+        String expData = "other name";
+        String data;
+        
+        this.attribute = new HtmlAttribute(DEFAULT_NAME);
+        
+        // Apply
+        this.attribute.setName(expData);
+        data = this.attribute.getName();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testSetValue_ReturnsCorrectValue() {
+        // Arrange
+        String expData = DEFAULT_VALUE;
+        String data;
+        
+        this.setState(DEFAULT_NAME);
+        
+        // Apply
+        this.attribute.setValue(expData);
+        data = this.attribute.getValue();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsFlag_WithNoValueSet_ExpectedTrue() {
+        // Arrange
+        boolean expData = true;
+        boolean data;
+        
+        this.setState(DEFAULT_NAME);
+        
+        // Apply
+        data = this.attribute.isFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsFlag_WithValueSet_ExpectedFalse() {
+        // Arrange
+        boolean expData = false;
+        boolean data;
+        
+        // Apply
+        data = this.attribute.isFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsClosingFlag_NonClosingTag_NamedFromConstructor_ResultIsFalse() {
+        // Arrange
+        boolean expData = false;
+        boolean data;
+        
+        // Apply
+        data = this.attribute.isClosingFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsClosingFlag_ClosingTag_NamedFromConstructor_ResultIsTrue() {
+        // Arrange
+        boolean expData = true;
+        boolean data;
+        
+        this.setState(CLOSING_NAME);
+        
+        // Apply
+        data = this.attribute.isClosingFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsClosingFlag_NonClosingTag_Set_ResultIsFalse() {
+        // Arrange
+        boolean expData = false;
+        boolean data;
+        
+        this.attribute.setName("some other name");
+        
+        // Apply
+        data = this.attribute.isClosingFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsClosingFlag_ClosingTag_Set_ResultIsTrue() {
+        // Arrange
+        boolean expData = true;
+        boolean data;
+        
+        this.attribute.setName(CLOSING_NAME);
+        
+        // Apply
+        data = this.attribute.isClosingFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testIsClosingFlag_StartWithClosingTag_SetNonClosing_ResultIsFalse() {
+        // Arrange
+        boolean expData = false;
+        boolean data;
+        
+        this.setState(CLOSING_NAME);
+        
+        this.attribute.setName(DEFAULT_NAME);
+        
+        // Apply
+        data = this.attribute.isClosingFlag();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testEquals_SameAttributes_ReusltIsTrue() {
+        // Arrange
+        boolean expData = true;
+        boolean data;
+        
+        HtmlAttribute other = new HtmlAttribute(DEFAULT_NAME, DEFAULT_VALUE);
+        
+        // Apply
+        data = this.getSymmetricEqualsPositive(other);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testEquals_DiffName_ResultIsFalse() {
+        // Arrange
+        boolean expData = false;
+        boolean data;
+        
+        HtmlAttribute other = new HtmlAttribute(DEFAULT_NAME + "diff", DEFAULT_VALUE);
+        
+        // Apply
+        data = this.getSymmetricEqualsNegative(other);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testEquals_DiffValue_ResultIsFalse() {
+        // Arrange
+        boolean expData = false;
+        boolean data;
+        
+        HtmlAttribute other = new HtmlAttribute(DEFAULT_NAME, DEFAULT_VALUE + "diff");
+        
+        // Apply
+        data = this.getSymmetricEqualsNegative(other);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testToString_StandardName_StandardValue() {
+        // Arrange
+        String expData = DEFAULT_NAME + HtmlAttribute.attributeSplitter + DEFAULT_VALUE;
+        String data;
+        
+        // Apply
+        data = this.attribute.toString();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testToString_StandardName_NoValue() {
+        // Arrange
+        String expData = DEFAULT_NAME;
+        String data;
+        
+        this.setState(expData);
+        
+        // Apply
+        data = this.attribute.toString();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testToString_NoName_StandardValue() {
+        // Arrange
+        String expData = HtmlAttribute.attributeSplitter + DEFAULT_VALUE;
+        String data;
+        
+        HtmlAttribute attr = new HtmlAttribute();
+        attr.setValue(DEFAULT_VALUE);
+        
+        // Apply
+        data = attr.toString();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testToString_NoName_NoValue() {
+        // Arrange
+        String expData = "";
+        String data;
+        
+        HtmlAttribute attr = new HtmlAttribute();
+        
+        // Apply
+        data = attr.toString();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testToString_ClosingName() {
+        // Arrange
+        String expData = HtmlAttribute.CLOSING_TAG;
+        String data;
+        
+        this.attribute.setName(CLOSING_NAME);
+        
+        // Apply
+        data = this.attribute.toString();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    private boolean getSymmetricEqualsPositive(HtmlAttribute other) {
+        return this.attribute.equals(other) &&
+               other.equals(this.attribute) &&
+               this.attribute.equals((Object) other) &&
+               other.equals((Object) this.attribute);
+    }
+    
+    private boolean getSymmetricEqualsNegative(HtmlAttribute other) {
+        return this.attribute.equals(other) ||
+               other.equals(this.attribute) ||
+               this.attribute.equals((Object) other) ||
+               other.equals((Object) this.attribute);
+    }
+    
+    private void setState(String name) {
+        this.attribute = new HtmlAttribute(name);
+    }
+    
+    @After
+    public void clearState() {
+        this.attribute = null;
+    }
+}
