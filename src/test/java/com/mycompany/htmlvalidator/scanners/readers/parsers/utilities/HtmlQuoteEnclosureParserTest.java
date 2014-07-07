1f23426d0776b8f91c0d4ae3508a6511d6e13f6f
Carl McGraw <cjmcgraw@uw.edu>
2014-07-07 03:52:25 -0700

    Branch Iss #2. Phase two, part one: Updated HtmlQuoteEnclosureParserTest to test for new functionality.

--- src/test/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/HtmlQuoteEnclosureParserTest.java
+++ src/test/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/HtmlQuoteEnclosureParserTest.java
@@ -2,91 +2,289 @@ package com.mycompany.htmlvalidator.scanners.readers.parsers.utilities;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
+import com.mycompany.htmlvalidator.scanners.MarkupTag;
 import com.mycompany.htmlvalidator.scanners.readers.parsers.MutableHtmlData;
-import com.mycompany.htmlvalidator.scanners.readers.parsers.errors.EndOfInputParsingException;
-import com.mycompany.htmlvalidator.scanners.readers.parsers.errors.MissingEnclosureParsingException;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.utilities.components.HtmlQuoteEnclosureParser;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.utilities.components.exceptions.MissingCharacterComponentException;
 import com.mycompany.htmlvalidator.scanners.readers.utilities.PushbackAndPositionReaderMock;
 
 public class HtmlQuoteEnclosureParserTest {
-    private static final List<Character> defaultData = Arrays.asList('"', 's', 'o', 'm', 'e', ' ', 'd', 'a', 't', 'a', '"', 'o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a');
+    private static final String DEFAULT_DATA_SINGLE_QUOTED = String.format("%sother data%s", MarkupTag.SINGLE_QUOTE, MarkupTag.SINGLE_QUOTE);
+    private static final String DEFAULT_DATA_DOUBLE_QUOTED = String.format("%ssome other data%s", MarkupTag.DOUBLE_QUOTE, MarkupTag.DOUBLE_QUOTE);
+    private static final String DEFAULT_REMAINING_DATA = "some remaining data";
     
     private HtmlQuoteEnclosureParser parser = new HtmlQuoteEnclosureParser();
     private PushbackAndPositionReaderMock input;
     private LinkedList<Character> inputData;
-    private MutableHtmlData result;
-    
-    @Before
-    public void setUp() {
-        this.setState(defaultData);
-    }
     
     @Test
-    public void testParse_WithValidData_GetMethodReturnsCorrectStringIncludingQuotes() throws IOException {
+    public void testParse_OneQuotedValue_ValidSingleQuotedValue_ReturnedDataMatches() throws IOException {
         // Arrange
-        String expData = "\"some data\"";
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
         String data;
         
+        this.setState(expData);
+        
         // Apply
-        this.parser.parse(this.input, this.result);
-        data = parser.getQuoteData();
+        data = this.parser.parse(this.input);
         
         // Assert
         assertEquals(expData, data);
     }
     
     @Test
-    public void testParse_WithValidData_ReturnsInputConsumesQuotesLeavesRemaining() throws IOException {
+    public void testParse_OneQuotedValueWithExtraRemainingData_ValidSingleQuotedValue_ReturnedDataMatches() throws IOException {
         // Arrange
-        String expData = "other data";
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
         String data;
         
+        this.setState(expData + DEFAULT_REMAINING_DATA);
+        
         // Apply
-        this.parser.parse(this.input, this.result);
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesSingleFollowedByDouble_ValidSingleQuotedValue_ReturnedDataMatches() throws IOException {
+        // Arrange
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
+        String data;
+        
+        this.setState(expData + DEFAULT_DATA_DOUBLE_QUOTED);
+        
+        // Apply
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesTwoSingle_ValidSingleQuotedValue_ReturnedDataMatches() throws IOException {
+        // Arrange
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
+        String data;
+        
+        this.setState(expData + expData);
+        
+        // Apply
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_OneQuotedValue_ValidSingleQuotedValue_RemainingDataIsEmpty() throws IOException {
+        // Arrange
+        String expData = "";
+        String data;
+        
+        this.setState(DEFAULT_DATA_SINGLE_QUOTED);
+        
+        // Apply
+        this.parser.parse(this.input);
         data = this.input.getRemainingData();
         
         // Assert
         assertEquals(expData, data);
     }
     
-    @Test(expected=EndOfInputParsingException.class)
-    public void testParse_WithInvalidData_EmptyData() throws IOException {
-        // Arrange
-        this.setState(new ArrayList<Character>());
+    @Test
+    public void testParse_OneQuotedValueWithExtraRemainingData_ValidSingleQuotedValue_ExpectedRemainingData() throws IOException {
+        String expData = DEFAULT_REMAINING_DATA;
+        String data;
         
-        // Apply + Assert
-        this.parser.parse(this.input, this.result);
-    }
-    
-    @Test(expected=MissingEnclosureParsingException.class)
-    public void testParse_WithInvalidData_MissingOpeningQuote() throws IOException {
-        // Arrange
-        this.setState(Arrays.asList('o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a', '"'));
+        this.setState(DEFAULT_DATA_SINGLE_QUOTED + expData);
         
-        // Apply + Assert
-        this.parser.parse(this.input, this.result);
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
     }
     
     @Test
-    public void testParse_WithInvalidData_MissingOpeningQuote_LeavesUnquotedDataAlone() throws IOException {
+    public void testParse_TwoQuotedValuesOneSingleFollowedByOneDouble_ValidSingleQuotedValue_ExpectedRemainingData() throws IOException {
+        String expData = DEFAULT_DATA_DOUBLE_QUOTED;
+        String data;
+        
+        this.setState(DEFAULT_DATA_SINGLE_QUOTED + expData);
+        
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesBothSingle_ValidSingleQuotedValue_ExpectedRemainingData() throws IOException {
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
+        String data;
+        
+        this.setState(DEFAULT_DATA_SINGLE_QUOTED + expData);
+        
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_OneQuotedValue_ValidDoubleQuotedValue_ReturnedDataMatches() throws IOException {
         // Arrange
-        this.setState(Arrays.asList('o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a', '"'));
-        String expData = "other data\"";
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
+        String data;
+        
+        this.setState(expData);
+        
+        // Apply
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_OneQuotedValueWithExtraRemainingData_ValidDoubleQuotedValue_ReturnedDataMatches() throws IOException {
+        // Arrange
+        String expData = DEFAULT_DATA_DOUBLE_QUOTED;
+        String data;
+        
+        this.setState(expData + DEFAULT_REMAINING_DATA);
+        
+        // Apply
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesSingleFollowedByDouble_ValidDoubleQuotedValue_ReturnedDataMatches() throws IOException {
+        // Arrange
+        String expData = DEFAULT_DATA_DOUBLE_QUOTED;
+        String data;
+        
+        this.setState(expData + DEFAULT_DATA_DOUBLE_QUOTED);
+        
+        // Apply
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesTwoSingle_ValidDoubleQuotedValue_ReturnedDataMatches() throws IOException {
+        // Arrange
+        String expData = DEFAULT_DATA_DOUBLE_QUOTED;
+        String data;
+        
+        this.setState(expData + expData);
+        
+        // Apply
+        data = this.parser.parse(this.input);
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_OneQuotedValue_ValidDoubleQuotedValue_RemainingDataIsEmpty() throws IOException {
+        // Arrange
+        String expData = "";
+        String data;
+        
+        this.setState(DEFAULT_DATA_DOUBLE_QUOTED);
+        
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_OneQuotedValueWithExtraRemainingData_ValidDoubleQuotedValue_ExpectedRemainingData() throws IOException {
+        String expData = DEFAULT_REMAINING_DATA;
+        String data;
+        
+        this.setState(DEFAULT_DATA_DOUBLE_QUOTED + expData);
+        
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesOneSingleFollowedByOneDouble_ValidDoubleQuotedValue_ExpectedRemainingData() throws IOException {
+        String expData = DEFAULT_DATA_DOUBLE_QUOTED;
+        String data;
+        
+        this.setState(DEFAULT_DATA_DOUBLE_QUOTED + expData);
+        
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test
+    public void testParse_TwoQuotedValuesBothSingle_ValidDoubleQuotedValue_ExpectedRemainingData() throws IOException {
+        String expData = DEFAULT_DATA_SINGLE_QUOTED;
+        String data;
+        
+        this.setState(DEFAULT_DATA_DOUBLE_QUOTED + expData);
+        
+        // Apply
+        this.parser.parse(this.input);
+        data = this.input.getRemainingData();
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    @Test(expected=MissingCharacterComponentException.class)
+    public void testParse_InvalidValueMissingStartQuote_ExpectedThrowsException() throws IOException {
+        // Arrange
+        this.setState(DEFAULT_REMAINING_DATA + MarkupTag.DOUBLE_QUOTE);
+        
+        // Apply + Assert
+        this.parser.parse(input);
+    }
+    
+    @Test
+    public void testParse_InvalidValueMissingStartQuoteQuote_AllDataRemainingInInput() throws IOException {
+        // Arrange
+        String expData = DEFAULT_REMAINING_DATA + MarkupTag.DOUBLE_QUOTE;
         String data = "";
+        
+        this.setState(expData);
         
         // Apply
         try {
-            this.parser.parse(this.input,  this.result);
-        } catch (MissingEnclosureParsingException e) {
+            this.parser.parse(input);
+        } catch (MissingCharacterComponentException e) {
             data = this.input.getRemainingData();
         }
         
@@ -95,43 +293,45 @@ public class HtmlQuoteEnclosureParserTest {
     }
     
     @Test
-    public void testParse_WithInvalidData_MissingOpeningQuote_GetMethodGivesEmptyString() throws IOException {
+    public void testParse_InvalidValueMissingStartQuote_StoredExceptionDataIsEmpty() throws IOException {
         // Arrange
-        this.setState(Arrays.asList('o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a', '"'));
         String expData = "";
-        String data = "default data to prevent simple pass";
+        String data = "filler to prevent false positive";
+        
+        this.setState(DEFAULT_REMAINING_DATA + MarkupTag.DOUBLE_QUOTE);
         
         // Apply
         try {
-            this.parser.parse(this.input, this.result);
-        } catch (MissingEnclosureParsingException e) {
-            data = this.parser.getQuoteData();
+            this.parser.parse(input);
+        } catch (MissingCharacterComponentException e) {
+            data = e.getData();
         }
         
         // Assert
         assertEquals(expData, data);
     }
     
-    @Test(expected=EndOfInputParsingException.class)
-    public void testParse_WithInvalidData_MissingClosingQuote() throws IOException {
+    @Test(expected=EOFException.class)
+    public void testParse_InvalidValueMissingEndDoubleQuote_ThrowsEOFException() throws IOException {
         // Arrange
-        this.setState(Arrays.asList('"', 'o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a'));
+        this.setState(MarkupTag.DOUBLE_QUOTE + DEFAULT_REMAINING_DATA);
         
         // Apply + Assert
-        this.parser.parse(this.input, this.result);
+        this.parser.parse(input);
     }
     
     @Test
-    public void testParse_WithInvalidData_MissingClosingQuote_ConsumesAllData() throws IOException {
+    public void testParse_InvalidValueMissingEndDoubleQuote_NoRemainingData() throws IOException {
         // Arrange
-        this.setState(Arrays.asList('"', 'o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a'));
         String expData = "";
-        String data = "some data to prevent simple pass";
+        String data = "value to prevent false positive";
+        
+        this.setState(MarkupTag.DOUBLE_QUOTE + DEFAULT_REMAINING_DATA);
         
         // Apply
         try {
-            this.parser.parse(this.input, this.result);
-        } catch (EndOfInputParsingException e) {
+            this.parser.parse(input);
+        } catch (EOFException e) {
             data = this.input.getRemainingData();
         }
         
@@ -139,34 +339,75 @@ public class HtmlQuoteEnclosureParserTest {
         assertEquals(expData, data);
     }
     
-    @Test
-    public void testParse_WithInvalidData_MissingClosingQuote_StringContainsAllData() throws IOException {
+    @Test(expected=EOFException.class)
+    public void testParse_SingleQuoteOpeningDoubleQuoteClosing_ThrowsEOFException() throws IOException {
         // Arrange
-        this.setState(Arrays.asList('"', 'o', 't', 'h', 'e', 'r', ' ', 'd', 'a', 't', 'a'));
-        String expData = "\"other data";
-        String data = "";
+        this.setState(MarkupTag.DOUBLE_QUOTE + DEFAULT_REMAINING_DATA + MarkupTag.SINGLE_QUOTE + DEFAULT_REMAINING_DATA);
+        
+        // Apply + Assert
+        this.parser.parse(input);
+    }
+    
+    @Test
+    public void testParse_SingleQuoteOpeningDoubleQuoteClosing_RemainingDataIsEmpty() throws IOException {
+        // Arrange
+        String expData = "";
+        String data = "value to prevent false positive";
+        
+        this.setState(MarkupTag.DOUBLE_QUOTE + DEFAULT_REMAINING_DATA + MarkupTag.SINGLE_QUOTE + DEFAULT_REMAINING_DATA);
         
         // Apply
         try {
-            this.parser.parse(this.input, this.result);
-        } catch (EndOfInputParsingException e) {
-            data = this.parser.getQuoteData();
+            this.parser.parse(input);
+        } catch (EOFException e) {
+            data = this.input.getRemainingData();
         }
         
         // Assert
         assertEquals(expData, data);
     }
     
-    private void setState(List<Character> data) {
-        this.inputData = new LinkedList<Character>(data);
+    @Test(expected=EOFException.class)
+    public void testParse_DoubleQuoteOpeningSingleQuoteClosing_ThrowsEOFException() throws IOException {
+        // Arrange
+        this.setState(MarkupTag.SINGLE_QUOTE + DEFAULT_REMAINING_DATA + MarkupTag.DOUBLE_QUOTE + DEFAULT_REMAINING_DATA);
+        
+        // Apply + Assert
+        this.parser.parse(input);
+    }
+    
+    @Test
+    public void testParse_DoubleQuoteOpeningSingleQuoteClosing_RemainingDataIsEmpty() throws IOException {
+        // Arrange
+        String expData = "";
+        String data = "value to prevent false positive";
+        
+        this.setState(MarkupTag.SINGLE_QUOTE + DEFAULT_REMAINING_DATA + MarkupTag.DOUBLE_QUOTE + DEFAULT_REMAINING_DATA);
+        
+        // Apply
+        try {
+            this.parser.parse(input);
+        } catch (EOFException e) {
+            data = this.input.getRemainingData();
+        }
+        
+        // Assert
+        assertEquals(expData, data);
+    }
+    
+    private void setState(String data) {
+        this.inputData = new LinkedList<Character>();
+        
+        for (int i = 0; i < data.length(); i++)
+            this.inputData.add(data.charAt(i));
+        
         this.input = new PushbackAndPositionReaderMock(this.inputData);
-        this.result = new MutableHtmlData();
+        new MutableHtmlData();
     }
     
     @After
     public void clearState() {
         this.inputData = null;
         this.input = null;
-        this.result = null;
     }
 }
