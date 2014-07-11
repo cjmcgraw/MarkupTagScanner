c389c34a1d430d23a4106d036852299125910b18
Carl McGraw <cjmcgraw@uw.edu>
2014-07-10 19:50:39 -0700

    Update: Updated HtmlComponentAttributeParser to remove redeclaration of abstract class and change super class inheritance.

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/HtmlComponentAttributeParser.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/HtmlComponentAttributeParser.java
@@ -1,12 +1,15 @@
 package com.mycompany.htmlvalidator.scanners.readers.parsers.utilities.components;
 
+import java.io.IOException;
+
 import com.mycompany.htmlvalidator.scanners.MarkupTag;
-import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlAttribute;
-import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlParser;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.*;
 import com.mycompany.htmlvalidator.scanners.readers.utilities.PushbackAndPositionReader;
 
-public abstract class HtmlComponentAttributeParser extends HtmlParser<HtmlAttribute> {
+public abstract class HtmlComponentAttributeParser extends MarkupParser<HtmlAttribute> {
     protected HtmlAttribute attribute;
+    
+    public abstract HtmlAttribute parse(PushbackAndPositionReader input) throws IOException;
     
     protected void setState(PushbackAndPositionReader input) {
         super.setState(input);
