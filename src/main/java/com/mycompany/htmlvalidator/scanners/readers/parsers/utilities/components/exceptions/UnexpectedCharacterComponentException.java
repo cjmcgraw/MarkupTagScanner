110a92697b0ea41676a6c33aa2fabc685dc664d5
Carl McGraw <cjmcgraw@uw.edu>
2014-07-18 03:04:53 -0700

    Branch Iss #2. Phase two - part four. Step five: Updated UnexpectedCharacterComponentException to include equality and hashcode methods as well as change arguments for default constructor to better match other ComponentException format. Provided test class to ensure proper functionality.

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/exceptions/UnexpectedCharacterComponentException.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/exceptions/UnexpectedCharacterComponentException.java
@@ -10,7 +10,7 @@ public class UnexpectedCharacterComponentException extends ComponentException {
     private char unexpected;
     private Object data;
     
-    public UnexpectedCharacterComponentException(Point position, char unexpected, Object data) {
+    public UnexpectedCharacterComponentException(char unexpected, Point position, Object data) {
         this.position = position;
         this.unexpected = unexpected;
         this.data = data;
@@ -25,4 +25,32 @@ public class UnexpectedCharacterComponentException extends ComponentException {
         String msg = String.format(UNEXPECTED_CHAR_MSG, this.position, this.unexpected);
         return String.format(DEFAULT_ERROR_MESSAGE, msg);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        result = prime * result + ((position == null) ? 0 : position.hashCode());
+        result = prime * result + unexpected;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        UnexpectedCharacterComponentException other = (UnexpectedCharacterComponentException) obj;
+        if (data == null) {
+            if (other.data != null) return false;
+        } else if (!data.equals(other.data)) return false;
+        if (position == null) {
+            if (other.position != null) return false;
+        } else if (!position.equals(other.position)) return false;
+        if (unexpected != other.unexpected) return false;
+        return true;
+    }
+    
+    
 }
