0b71a507345374a17f1bdf21edff69e2e0f87be4
Carl McGraw <cjmcgraw@uw.edu>
2014-07-18 03:16:27 -0700

    Branch Iss #2. Phase two - part four. Step 13: Updated InputParser to allow for state exception checking.

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/InputParser.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/InputParser.java
@@ -2,9 +2,14 @@ package com.mycompany.htmlvalidator.scanners.readers.parsers;
 
 import java.awt.Point;
 import java.io.*;
+
+import com.mycompany.htmlvalidator.scanners.readers.parsers.exceptions.InvalidStateException;
 import com.mycompany.htmlvalidator.scanners.readers.utilities.PushbackAndPositionReader;
 
 public abstract class InputParser<T> {
+    private static final String CLASS_NAME = "InputParser";
+    private static final String FIELD_NAME = "input";
+    
     protected PushbackAndPositionReader input;
     
     public abstract T parse(PushbackAndPositionReader input) throws IOException;
@@ -18,6 +23,7 @@ public abstract class InputParser<T> {
     }
     
     protected char read() throws IOException {
+       this.validateState();
         int value = this.input.read();
         
         if (value == -1)
@@ -27,14 +33,26 @@ public abstract class InputParser<T> {
     }
     
     protected void unread(char ch) throws IOException {
+        this.validateState();
         this.input.unread(ch);
     }
     
     protected Point currentPosition() {
+        this.validateState();
         return this.input.getPosition();
     }
     
     protected boolean isWhitespace(char c) {
         return Character.isWhitespace(c);
+    }
+    
+    private void validateState() {
+        if (this.isMissingState()) {
+            throw new InvalidStateException(CLASS_NAME, FIELD_NAME);
+        }
+    }
+    
+    private boolean isMissingState() {
+        return this.input == null;
     }
 }
