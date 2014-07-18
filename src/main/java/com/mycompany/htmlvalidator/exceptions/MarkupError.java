b0e5f336654ddb711fb8385c9355af020e939cf8
Carl McGraw <cjmcgraw@uw.edu>
2014-07-18 03:00:03 -0700

    Branch Iss #2. Phase two - part four. Step one: Created MarkupError and MarkupErrorReporter interfaces that will represent the basic functionaltiy of Errors and Reporters.

---
+++ src/main/java/com/mycompany/htmlvalidator/exceptions/MarkupError.java
@@ -0,0 +1,10 @@
+package com.mycompany.htmlvalidator.exceptions;
+
+public interface MarkupError {
+    
+    public String toString();
+    
+    public boolean equals(Object other);
+    
+    public int hashCode();
+}
