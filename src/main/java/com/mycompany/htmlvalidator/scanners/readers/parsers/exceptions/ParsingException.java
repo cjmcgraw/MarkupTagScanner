e1ed37e0205aa4274e70978930b9ab4d884ccae9
Carl McGraw <cjmcgraw@uw.edu>
2014-07-27 17:18:07 -0700

    Update: Removed unnecessary coupling between ParsingException and HtmlData

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/exceptions/ParsingException.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/exceptions/ParsingException.java
@@ -1,15 +1,11 @@
 package com.mycompany.htmlvalidator.scanners.readers.parsers.exceptions;
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlAttribute;
 import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlData;
 
-public class ParsingException extends RuntimeException implements HtmlData{
+public class ParsingException extends RuntimeException {
     private static final long serialVersionUID = 7976665383247960546L;
-    private static final String errorMsg = "-----> PARSING ERROR  at [%s, %s] input=%s : %s at %s";
+    private static final String errorMsg = "-----> PARSING ERROR  at [%s, %s] tinput = %s%n\t%s at %s";
     private static final int numOfMsgArgs = 5;
     
     private Point position;
@@ -41,43 +37,6 @@ public class ParsingException extends RuntimeException implements HtmlData{
     public char getErrorCharacter() {
         return this.errorChar;
     }
-
-    @Override
-    public String getName() {
-        return this.getNameHelper();
-    }
-    
-    private String getNameHelper() {
-        return (this.validHtmlData()) ? this.htmlData.getName() : "";
-    }
-
-    @Override
-    public List<HtmlAttribute> getAttributes() {
-        return this.getDataHelper();
-    }
-    
-    private List<HtmlAttribute> getDataHelper() {
-        return (this.validHtmlData()) ? this.htmlData.getAttributes() : new ArrayList<HtmlAttribute>();
-    }
-
-    @Override
-    public boolean isClosing() {
-        return this.isClosingHelper();
-    }
-    
-    @Override
-    public boolean hasOpeningTag() {
-        return this.htmlData.hasOpeningTag();
-    }
-
-    @Override
-    public boolean hasClosingTag() {
-        return this.htmlData.hasClosingTag();
-    }
-    
-    private boolean isClosingHelper() {
-        return this.validHtmlData() && this.htmlData.isClosing();
-    }
     
     public boolean validHtmlData() {
         return this.htmlData != null;
@@ -104,10 +63,5 @@ public class ParsingException extends RuntimeException implements HtmlData{
     private String getHtmlDataToString() {
         String missingMsg = "**MISSING**";
         return (this.validHtmlData()) ? this.htmlData.toString() : missingMsg;
-    }
-
-    @Override
-    public boolean isSelfClosing() {
-        return true;
     }
 }
