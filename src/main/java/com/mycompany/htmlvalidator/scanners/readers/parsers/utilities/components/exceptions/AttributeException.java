04d93dab36d20cdc7995d038985d8145c370f34e
Carl McGraw <cjmcgraw@uw.edu>
2014-09-05 19:00:12 -0700

    Added AttributeException abstract base class, and EndOfInputAttributeException concrete class that is used for passing attribute data after an error.

---
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/exceptions/AttributeException.java
@@ -0,0 +1,21 @@
+package com.mycompany.htmlvalidator.scanners.readers.parsers.utilities.components.exceptions;
+
+import com.mycompany.htmlvalidator.exceptions.MarkupError;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlAttribute;
+
+public abstract class AttributeException extends RuntimeException implements MarkupError{
+    private static final long serialVersionUID = 2298941335061905710L;
+    protected static final String DEFAULT_ERROR_MSG = "ERROR: Exception during attribute parsing -> %s";
+    
+    protected AttributeException(String msg) {
+        super(msg);
+    }
+    
+    public abstract HtmlAttribute getAttribute();
+    
+    public abstract String getErrorMessage();
+    
+    public String toString() {
+        return getErrorMessage();
+    }
+}
