92fe9c881c270eb735199a6dfc3183eb99519465
Carl McGraw <cjmcgraw@uw.edu>
2014-09-08 01:52:40 -0700

    Updated Mock objects to allow for throwing of their respective errors.

--- src/test/mock/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/HtmlSingleAttributeParserMock.java
+++ src/test/mock/java/com/mycompany/htmlvalidator/scanners/readers/parsers/utilities/components/HtmlSingleAttributeParserMock.java
@@ -3,18 +3,35 @@ package com.mycompany.htmlvalidator.scanners.readers.parsers.utilities.component
 import java.io.IOException;
 
 import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlAttribute;
+import com.mycompany.htmlvalidator.scanners.readers.parsers.utilities.components.exceptions.AttributeException;
 import com.mycompany.htmlvalidator.scanners.readers.utilities.PushbackAndPositionReader;
 
 public class HtmlSingleAttributeParserMock extends HtmlSingleAttributeParser {
     private HtmlAttribute result;
+    private AttributeException err;
     
     public HtmlSingleAttributeParserMock(String result) {
         this.result = new HtmlAttribute(result);
     }
     
+    public HtmlSingleAttributeParserMock(String result, AttributeException err) {
+        this(result);
+        this.err = err;
+    }
+    
+    public void setError(AttributeException err) {
+        this.err = err;
+    }
+    
+    public AttributeException getError() {
+        return this.err;
+    }
+    
     @Override
     public HtmlAttribute parse(PushbackAndPositionReader input) throws IOException {
         input.read();
+        if (err != null)
+            throw this.err;
         return this.result;
     }
 }
