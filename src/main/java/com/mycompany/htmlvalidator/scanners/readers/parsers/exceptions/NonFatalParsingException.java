3f9099a2adcf75e75da0ccb1981b683d46d177ae
Carl McGraw <cjmcgraw@uw.edu>
2014-09-20 04:51:23 -0700

    Updated NonFatalParsingException to reduce whitespace to increase visibility

--- src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/exceptions/NonFatalParsingException.java
+++ src/main/java/com/mycompany/htmlvalidator/scanners/readers/parsers/exceptions/NonFatalParsingException.java
@@ -7,8 +7,7 @@ import com.mycompany.htmlvalidator.scanners.readers.parsers.HtmlData;
 public class NonFatalParsingException extends ParsingException {
     private static final long serialVersionUID = 249335334748494657L;
 
-    public NonFatalParsingException(Point position, HtmlData htmlData,
-            char errorChar, String msg) {
+    public NonFatalParsingException(Point position, HtmlData htmlData, char errorChar, String msg) {
         super(position, htmlData, errorChar, msg);
     }
     
