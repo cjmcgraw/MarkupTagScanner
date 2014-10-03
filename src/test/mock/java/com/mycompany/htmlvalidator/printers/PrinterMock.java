37ffcc0007d89959125fe374589b5cb1a49cd7a1
Carl McGraw <cjmcgraw@uw.edu>
2014-10-03 11:18:39 -0700

    Updated mock classes

---
+++ src/test/mock/java/com/mycompany/htmlvalidator/printers/PrinterMock.java
@@ -0,0 +1,54 @@
+/*
+ * @author: Carl McGraw
+ * @email:  cjmcgraw@u.washington.edu
+ * @version: 1.0
+ */
+
+package com.mycompany.htmlvalidator.printers;
+
+import java.io.PrintStream;
+import java.util.*;
+
+public class PrinterMock implements Printer{
+    public static final String NEWLINE = String.format("%n");
+    private List<String> printerData;
+
+
+    public PrinterMock() {
+        this.printerData = new ArrayList<>();
+    }
+
+    @Override
+    public void addPrinter(PrintStream printer) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterable<PrintStream> getPrinters() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void removePrinter(PrintStream printer) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void print(String s) {
+        printerData.add(s);
+    }
+
+    @Override
+    public void println() {
+        printerData.add(NEWLINE);
+    }
+
+    @Override
+    public void println(String s) {
+        printerData.add(s + NEWLINE);
+    }
+
+    public List<String> getPrintData() {
+        return printerData;
+    }
+}
