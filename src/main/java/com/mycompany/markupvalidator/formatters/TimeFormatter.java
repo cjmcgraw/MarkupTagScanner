fb015804aff3c10ac6c8f95ce967bc79aea21973
Carl McGraw <cjmcgraw@uw.edu>
2014-10-14 01:36:10 -0700

    Updated TimeFormatter class to include functionality

--- src/main/java/com/mycompany/markupvalidator/formatters/TimeFormatter.java
+++ src/main/java/com/mycompany/markupvalidator/formatters/TimeFormatter.java
@@ -6,5 +6,61 @@
 
 package com.mycompany.markupvalidator.formatters;
 
-public class TimeFormatter {
+public class TimeFormatter extends Formatter{
+    public static final String TIME_FORMAT = "It took %01d minutes and %.4f seconds to execute!";
+    public static final double NS_DENOMINATOR = Math.pow(10, 9);
+
+    private double ns;
+
+    protected void setState(double ns) {
+        super.setState();
+        this.ns = ns;
+    }
+
+    protected void clearState() {
+        super.clearState();
+        this.ns = 0.0;
+    }
+
+    public String format(Object obj) {
+        setState((double) obj);
+        String result = generateTimeFormat();
+        clearState();
+        return result;
+    }
+
+    private String generateTimeFormat() {
+        createHeader();
+        createBody();
+        createFooter();
+        return format();
+    }
+
+    private void createHeader() {
+        writeln();
+        writeln(BORDER_DASH);
+        writeln();
+    }
+
+    private void createBody() {
+        int mins = (int) getTotalMinutes();
+        double secs = getTotalSeconds() % 60;
+        writef(TIME_FORMAT, mins, secs);
+        writeln();
+    }
+
+    private double getTotalMinutes() {
+        return getTotalSeconds() / 60.0;
+    }
+
+    private double getTotalSeconds() {
+        return ns / NS_DENOMINATOR;
+    }
+
+    private void createFooter() {
+        writeln();
+        writeln(BORDER_DASH);
+        writeln();
+    }
+
 }
