be210f0a0df5e18203771cfc3b92245d7d064f9a
Carl McGraw <cjmcgraw@uw.edu>
2014-10-17 02:39:49 -0700

    Refactoring: Added GPLv3 clause to the top of every file.

--- src/main/java/com/mycompany/markupvalidator/errors/InvalidStateException.java
+++ src/main/java/com/mycompany/markupvalidator/errors/InvalidStateException.java
@@ -1,6 +1,19 @@
+/*  This file is part of MarkupValidator.
+ *
+ *  MarkupValidator is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MarkupValidator is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MarkupValidator. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mycompany.markupvalidator.errors;
-
-import com.mycompany.markupvalidator.errors.MarkupError;
 
 public class InvalidStateException extends MarkupError {
     private static final long serialVersionUID = 7342202150355963466L;
--- src/main/java/com/mycompany/markupvalidator/formatters/ReportFormatter.java
+++ src/main/java/com/mycompany/markupvalidator/formatters/ReportFormatter.java
@@ -1,7 +1,17 @@
-/*
- * @author: Carl McGraw
- * @email:  cjmcgraw@u.washington.edu
- * @version: 1.0
+/*  This file is part of MarkupValidator.
+ *
+ *  MarkupValidator is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MarkupValidator is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MarkupValidator. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.mycompany.markupvalidator.formatters;
@@ -9,8 +19,6 @@ package com.mycompany.markupvalidator.formatters;
 import com.mycompany.markupvalidator.MarkupTagScanners.Tag;
 import com.mycompany.markupvalidator.errors.MarkupError;
 import com.mycompany.markupvalidator.reports.Report;
-
-import java.awt.*;
 
 public class ReportFormatter extends Formatter<Report> {
     public static final String BODY_FORMAT_SECTION_1 = "Total Errors:%n%n" +
